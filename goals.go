62fab7071fbcf6bd6340578cbe03fe75fda65e1a
Patrick Arminio <patrick.arminio@gmail.com>
2015-12-24 14:11:59 +0100

    Initial commit

---
+++ goals.go
@@ -0,0 +1,14 @@
+package goals
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "This should be my goals app!")
+}
