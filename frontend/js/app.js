3ab3695dcfaef1a88269649b8e46e8727f0acc5e
Patrick Arminio <patrick.arminio@gmail.com>
2016-01-02 20:38:38 +0100

    Added source sans pro

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import FontFaceObserver from 'fontfaceobserver';
+
+
 import '../css/main.css';
+
+
+// Observer loading of Source Sans Pro
+const openSansObserver = new FontFaceObserver('Source Sans Pro', {});
+
+// When Open Sans is loaded, add the js-open-sans-loaded class to the body
+openSansObserver.check().then(() => {
+  document.body.classList.add('js-source-sans-pro-loaded');
+}, () => {
+  document.body.classList.remove('js-source-sans-pro-loaded');
+});
 
 
 ReactDOM.render(
